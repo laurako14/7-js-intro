60a0d340dae80a211f979e505e5ee2f7a3e70123
LauraKo <kovarskyte@yahoo.com>
2020-11-11 16:44:40 +0200

    backtick introduction

--- js/strings.js
+++ js/strings.js
@@ -11,3 +11,19 @@ console.log(father);
 
 // vietoj enterio \, ` ` ignoruoja ' ir enterius 
 
+
+const berniukas = 'Joniukas';
+const mergaite = 'Gretute';
+const veiksmas = 'ejo';
+const vieta = 'miska';
+
+const pasaka = `${berniukas} ir ${mergaite} ${veiksmas} i ${vieta}.`;
+console.log(pasaka);
+
+const heroName = 'Chuck';
+const heroLastname = 'Norris';
+const birth = 1940;
+
+const heroSays = `${heroName} ${heroLastname} (${2020-birth}).`
+console.log(heroSays);
+
