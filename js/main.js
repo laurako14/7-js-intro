4219f5c80156d6423fc6897f656ea8ac41af57bc
LauraKo <kovarskyte@yahoo.com>
2020-11-11 16:15:05 +0200

    tekstu pavyzdziai

---
+++ js/main.js
@@ -0,0 +1,60 @@
+console.log(8416584);
+
+console.log(3, 14);
+console.log(3.14);
+
+console.log('Labas rytas');
+console.log("Labas rytas");
+
+const pirmasSkaicius = 2;
+const antrasSkaicius = 3;
+
+console.log("Pirmas skaicius turetu buti 2.");
+console.log(pirmasSkaicius);
+
+console.log("Antras skaicius turetu buti 3.");
+console.log(antrasSkaicius);
+
+const suma = pirmasSkaicius + antrasSkaicius;
+
+console.log(suma);
+
+console.log(pirmasSkaicius, "+", antrasSkaicius, "=", suma);
+
+const vardas = 'Laura';
+const amzius = 99;
+console.log(vardas);
+console.log(amzius);
+
+const sakinys = vardas + ' yra ' + amzius + ' metu amziaus.';
+console.log(sakinys);
+
+const suo = 'Rikis';
+const maistas = 'kaulas';
+
+const ssakinys = suo + ' megstamas maistas yra ' + maistas + '.';
+console.log(ssakinys);
+
+const heroName = 'Chuck';
+const heroLastname = 'Norris';
+const birth = 1940;
+
+const skaiciavimas = heroName + ' ' + heroLastname + ' (' + (2020 - birth) + ')';
+console.log(skaiciavimas);
+
+console.clear();
+
+let pinigine = 0;
+const sausis = 100;
+const vasaris = 200;
+const kovas = 70;
+
+pinigine = pinigine + sausis;
+console.log(pinigine);
+
+pinigine = pinigine + vasaris;
+console.log(pinigine);
+
+pinigine = pinigine + kovas;
+console.log(pinigine);
+
---
+++ js/strings.js
@@ -0,0 +1,13 @@
+const vardas = 'Vardenis';
+const pasisveikinimas = "Labas rytas, Lietuva!";
+
+console.log(pasisveikinimas);
+
+const mother = "Mother said: \"Don't do it!\".";
+const father = 'Fahter said: "Don\'t do it!".';
+
+console.log(mother);
+console.log(father);
+
+// vietoj enterio \, ` ` ignoruoja ' ir enterius 
+
